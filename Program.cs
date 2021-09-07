b5242c8275b7217bd7ca562946d3b9540dff8157
arianastudent09 <A.Voloshyna.FIT.121.20@knute.edu.ua>
2021-09-07 08:59:17 +0300

    second commit

--- Program.cs
+++ Program.cs
@@ -6,7 +6,8 @@ namespace laba1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Hello KNUTE!");
         }
     }
 }
